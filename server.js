4cd7ad004d0b577993344d4f1088a3a5908f4547
Suhail <mohammedsuhail100506@gmail.com>
2025-12-19 12:02:37 +0530

    Fix CORS configuration for Vercel deployment

--- server.js
+++ server.js
@@ -3,22 +3,22 @@ require('dotenv').config();
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const connectDB = require('./config/db');
-const cors = require('cors');
 const app = express();
 
 connectDB();
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
 
 const corsOptions = {
     origin: ['https://job-portal-frontend-seven-omega.vercel.app', 'http://localhost:5173'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    exposedHeaders: ['Content-Type', 'Authorization']
 };
+
 app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.use('/api/auth', require('./Routes/authRoutes'));
 app.use('/api/jobs', require('./Routes/jobRoutes'));
